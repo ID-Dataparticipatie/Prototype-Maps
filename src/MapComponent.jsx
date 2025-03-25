a1524a08f1bc5fb7bc9f72f8340172615c7954d1
Justwouter <wouter2312@gmail.com>
2025-03-25 10:21:16 +0100

    Jessie's changes

---
+++ src/MapComponent.jsx
@@ -0,0 +1,31 @@
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
+
+
+
+const MapComponent = () => {
+  const position = [52.0705, 4.3007]; // Coördinaten van Den Haag
+
+  
+
+  return (
+    <MapContainer center={position} zoom={13} className="leaflet-container">
+      
+      <TileLayer
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+      />
+      <Marker position={position}>
+        <Popup>Welkom in Den Haag team data & participatie!</Popup>
+      </Marker>
+    </MapContainer>
+
+    
+
+    
+  );
+};
+
+
+
+export default MapComponent;
