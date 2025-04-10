6b8e749c20e520bc4bb365815ef910b3d6f24db2
Justwouter <wouter2312@gmail.com>
2025-04-10 10:26:20 +0200

    Add map bounds

--- src/components/Map/MapComponent.tsx
+++ src/components/Map/MapComponent.tsx
@@ -1,4 +1,10 @@
-import { MapContainer, TileLayer, Marker, Popup, useMapEvents } from "react-leaflet";
+import {
+  MapContainer,
+  TileLayer,
+  Marker,
+  Popup,
+  useMapEvents,
+} from "react-leaflet";
 import { useState, useEffect } from "react";
 import L from "leaflet";
 import "leaflet/dist/leaflet.css";
@@ -14,6 +20,11 @@ const customIcon = new L.Icon({
   iconAnchor: [12, 41],
 });
 
+const bounds = L.latLngBounds([
+  [-85, -180], // Southwest
+  [85, 180], // Northeast
+]);
+
 interface TestMarker {
   id: number;
   lat: number;
@@ -24,17 +35,15 @@ interface TestMarker {
 function MapComponent() {
   const [markers, setMarkers] = useState<TestMarker[]>([]);
 
-
   useEffect(() => {
-    const savedMarkers = JSON.parse(localStorage.getItem("markers") ?? "") || [];
+    const savedMarkers =
+      JSON.parse(localStorage.getItem("markers") ?? "") || [];
     setMarkers(savedMarkers);
   }, []);
-
 
   useEffect(() => {
     localStorage.setItem("markers", JSON.stringify(markers));
   }, [markers]);
-
 
   function MapClickHandler() {
     useMapEvents({
@@ -45,7 +54,17 @@ function MapComponent() {
           lng: e.latlng.lng,
           text: "",
         };
-        if(markers.find(marker => calcDistance(marker.lat, marker.lng, newMarker.lat, newMarker.lng) < 0.0001)) {
+        if (
+          markers.find(
+            (marker) =>
+              calcDistance(
+                marker.lat,
+                marker.lng,
+                newMarker.lat,
+                newMarker.lng
+              ) < 0.0001
+          )
+        ) {
           return;
         }
         setMarkers((prevMarkers) => [...prevMarkers, newMarker]);
@@ -54,13 +73,12 @@ function MapComponent() {
     return null;
   }
 
-  function calcDistance(x1:number,y1:number,x2:number,y2:number) {
+  function calcDistance(x1: number, y1: number, x2: number, y2: number) {
     // Standard pythagorean theorem
-    return Math.sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1));
+    return Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
   }
 
-
-  const updateMarkerText = (id:number, newText:string) => {
+  const updateMarkerText = (id: number, newText: string) => {
     setMarkers((prevMarkers) =>
       prevMarkers.map((marker) =>
         marker.id === id ? { ...marker, text: newText } : marker
@@ -68,28 +86,38 @@ function MapComponent() {
     );
   };
 
-
-  const removeMarker = (id:number) => {
-    setMarkers((prevMarkers) => prevMarkers.filter((marker) => marker.id !== id));
+  const removeMarker = (id: number) => {
+    setMarkers((prevMarkers) =>
+      prevMarkers.filter((marker) => marker.id !== id)
+    );
   };
 
   return (
     <div className="flex items-center justify-center">
-      <MapContainer 
+      <MapContainer
         center={[52.0667905, 4.3234636]}
         zoom={25}
-        // scrollWheelZoom={false}
         preferCanvas={true}
-        className="h-[91vh] w-full overflow-hidden">
+        className="h-[91vh] w-full overflow-hidden"
+        // Locks the map to disallow scrolling outside of the map view.
+        maxBounds={bounds}
+        maxBoundsViscosity={1.0}
+        minZoom={3}
+      >
         <TileLayer
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          noWrap={true}
         />
 
         <MapClickHandler />
 
         {markers.map((marker) => (
-          <Marker key={marker.id} position={[marker.lat, marker.lng]} icon={customIcon}>
+          <Marker
+            key={marker.id}
+            position={[marker.lat, marker.lng]}
+            icon={customIcon}
+          >
             <Popup>
               <input
                 type="text"
